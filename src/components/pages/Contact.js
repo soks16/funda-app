f3c4be71adbfdc8bb4a6279ed913f32e8a38e9e6
Jesus SOKAMBY <sokambyj@meeexpertise.com>
2025-03-02 21:38:54 +0000

    [ADD] ajout de fonctionalité Recaptcha et send form mail contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from 'sweetalert2';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 function Contactus(){
+    const [capVal, setCapVal] = useState(null);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "4fc6ac59-90fe-4ed4-b0e5-8555f6aeb0eb");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Success !",
+                text: "Message send successfully!",
+                icon: "success"
+            });
+            event.target.reset();
+        }
+    };
+
     return(
         <div>
             <section className="py-4 bg-info">
@@ -21,28 +52,31 @@ function Contactus(){
                         <div className="card-body">
                             <div className="row">
                                 <div className="col-md-6">
+                                    <form onSubmit={onSubmit}>
                                     <h6>Contact Form</h6>
                                     <hr/>
                                     <div className="form-group">
                                         <label className="mb-1">Full name</label>
-                                        <input type="text" className="form-control" name="" placeholder="Enter full name"/>
+                                        <input type="text" className="form-control" name="name" placeholder="Enter full name" required/>
                                     </div>
                                     <div className="form-group">
                                         <label className="mb-1">Phone Number</label>
-                                        <input type="text" className="form-control" name="" placeholder="Enter Phone Number"/>
+                                        <input type="text" className="form-control" name="phone" placeholder="Enter Phone Number" required/>
                                     </div>
                                     <div className="form-group">
                                         <label className="mb-1">Email Adress</label>
-                                        <input type="email" className="form-control" name="" placeholder="Enter Email Adress"/>
+                                        <input type="email" className="form-control" name="email" placeholder="Enter Email Adress" required/>
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group mb-2">
                                         <label className="mb-1">Message</label>
-                                        <textarea rows="5" className="form-control" placeholder="Type your message">
+                                        <textarea rows="5" className="form-control" name="message" placeholder="Type your message" required>
                                         </textarea>
                                     </div>
+                                    <ReCAPTCHA sitekey="6LfgeecqAAAAANXFDA-0zIhKPrx0czrOYSZo1l5J" onChange={(val) => setCapVal(val)}/>
                                     <div className="form-group py-3">
-                                        <button type="button" className="btn btn-primary shadow w-100">Send Message</button>
+                                        <button type="submit" disabled={!capVal} className="btn btn-primary shadow w-100">Send Message</button>
                                     </div>
+                                    </form>
                                 </div>
                                 <div className="col-md-6 border-start">
                                     <h5>Address Information</h5>
